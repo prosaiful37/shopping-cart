2c374006b1dd8ea4c02e885450495431f022156b
Saiful Islam <57891501+prosaiful37@users.noreply.github.com>
2022-02-15 00:02:52 +0600

    project iphone case section done

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
-document.getElementById('case-plus').addEventListener('click', function(){
-
-    // input filed value add
+// common function update input value
+function updateCaseNumber(isIncreasing){
     const caseInput = document.getElementById('case-number');
-    const caseNumber = caseInput.value;
-    caseInput.value = parseInt(caseNumber) + 1 ;
-    
-})
+    let caseNumber = caseInput.value;
+
+    if(isIncreasing == true){
+        caseNumber = parseInt(caseNumber) + 1;
+    }
+    else if(caseNumber > 0){
+        caseNumber  = parseInt(caseNumber) - 1;
+    }
+    caseInput.value = caseNumber;
+
+    // update case total
+    const caseTotal = document.getElementById('case-total');
+    caseTotal.innerText = caseNumber * 59;
+  
+}
+
+// handle case incresing degree event
+document.getElementById('case-plus').addEventListener('click', function(){
+    updateCaseNumber(true);
+});
+// case minuse 
+document.getElementById('case-minus').addEventListener('click', function(){
+    updateCaseNumber(false);
+});
