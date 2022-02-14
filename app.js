f9b7813fec7e509f95798facb419a65115aef73e
Saiful Islam <57891501+prosaiful37@users.noreply.github.com>
2022-02-14 17:24:57 +0600

    pluse input field value set

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 document.getElementById('case-plus').addEventListener('click', function(){
+
+    // input filed value add
+    const caseInput = document.getElementById('case-number');
+    const caseNumber = caseInput.value;
+    caseInput.value = parseInt(caseNumber) + 1 ;
     
 })
